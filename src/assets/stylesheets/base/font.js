9083bccd6c41a8b4968df9d28154a4769551a793
hengsokly <hengsokly23@gmail.com>
2026-01-15 15:09:54 +0700

    add assets

---
+++ src/assets/stylesheets/base/font.js
@@ -0,0 +1,11 @@
+export const FontFamily = {
+  body: 'KhmerOSContent',
+  title: 'KhmerOSContent-Bold'
+};
+
+export const FontSize = {
+  body: 16,
+  title: 28,
+  small: 14,
+  hintTitle: 20,
+};
---
+++ src/assets/stylesheets/base_style.js
@@ -0,0 +1,3 @@
+export { default as Color } from './base/color';
+export { FontSize, FontFamily } from './base/font';
+export { default as Style } from './base/style';
