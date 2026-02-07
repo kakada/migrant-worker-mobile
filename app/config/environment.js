53d7761fef77b6d7d755286d84a29f951c49045b
Kimsanlim <kimsanlim27@gmail.com>
2026-02-07 11:00:28 +0700

    Resolve the warning of the deprecated use of the Firebase Analytics

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,8 +1,9 @@
 export const environment = {
-  apiUrl: 'http://192.168.1.140:3000/api/v2',
-  serverUrl: 'http://192.168.1.140:3000',
+  apiUrl: 'https://myjourneys-stg.dojoconnect.co/api/v2',
+  serverUrl: 'https://myjourneys-stg.dojoconnect.co/',
   state: 'development',
-  access_token: '5bbc3ffaf6e725da9d8e3092340e5d1c',
+  // access_token: '5bbc3ffaf6e725da9d8e3092340e5d1c',
+  access_token: '87c0aa1844857658245e10ed614c12d9',
   forward_rewind_audio_duration: 3,
   youtube_api_key: '',
 };
