9baf81e1fffc715438ab1702f4281aab58ec95de
Kimsanlim <kimsanlim27@gmail.com>
2026-03-05 23:32:43 +0700

    Ignore the environment file

--- app/config/environment.js
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  apiUrl: 'https://myjourneys-stg.dojoconnect.co/api/v2',
-  serverUrl: 'https://myjourneys-stg.dojoconnect.co/',
-  state: 'development',
-  // access_token: '5bbc3ffaf6e725da9d8e3092340e5d1c',
-  access_token: '87c0aa1844857658245e10ed614c12d9',
-  forward_rewind_audio_duration: 3,
-  youtube_api_key: '',
-};
